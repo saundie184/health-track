63bfdf0857c149d6febdedb1c664bf07204f7c5a
Saundie Weiss <saundieweiss@gmail.com>
2016-03-29 18:21:39 -0600

    Changed accent color, moved layout on homepage, and made other design changes.

--- app/js/routes.js
+++ app/js/routes.js
@@ -55,12 +55,12 @@ app.config(function($routeProvider, $httpProvider){
 app.config(function($mdThemingProvider) {
   $mdThemingProvider.theme('default')
     .primaryPalette('light-blue',  {
-      'default': '500', // by default use shade 400 from the pink palette for primary intentions
+      'default': '600', // by default use shade 400 from the pink palette for primary intentions
       'hue-1': '700', // use shade 100 for the <code>md-hue-1</code> class
       'hue-2': '800', // use shade 600 for the <code>md-hue-2</code> class
       'hue-3': '900' // use shade A100 for the <code>md-hue-3</code> class
     })
     .accentPalette('red', {
-      'default': '600'
+      'default': 'A200'
     });
 });
